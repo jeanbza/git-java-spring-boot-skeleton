2edf782ce517106608d080366acaf30237257c98
Jean de Klerk <jadekler@gmail.com>
2015-09-16 23:20:44 -0600

    Working gulp

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -5,6 +5,10 @@ var imagemin = require('gulp-imagemin');
 var sourcemaps = require('gulp-sourcemaps');
 var del = require('del');
 
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+
 var paths = {
     external_scripts: [
         'node_modules/jquery/dist/jquery.min.js'
@@ -17,32 +21,17 @@ gulp.task('clean', function() {
     return del(['dist']);
 });
 
-gulp.task('scripts', ['clean'], function() {
-    return gulp.src(paths.application_scripts)
-        .pipe(sourcemaps.init())
-        .pipe(uglify())
-        .pipe(concat('all.min.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('dist/js'));
-});
-
-gulp.task('vendor-javascript', function() {
-    gulp.src(['node_modules/**/*.js'])
-        .pipe(sourcemaps.init())
-        .pipe(concat('vendor.min.js'))
-        .pipe(gulp.dest('dist/js'));
-});
-
 gulp.task('images', ['clean'], function() {
     return gulp.src(paths.images)
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('browserify-js', [], function () {
-    return browserify('dist/js/all.min.js').bundle()
-        // vinyl-source-stream makes the bundle compatible with gulp
-        .pipe(source('dist/js/all.min.js'))
-        .pipe(gulp.dest('dist/js/all.min.js'));
+gulp.task('browserify:js', [], function () {
+    return browserify('js/greeting.js').bundle()
+        .pipe(source('all.min.js'))
+        .pipe(buffer())
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('default', ['vendor-javascript', 'scripts', 'images']);
+gulp.task('default', ['images', 'browserify:js']);
--- client/js/greeting.js
+++ client/js/greeting.js
@@ -1,4 +1,4 @@
-require('jquery');
+var $ = require('jquery');
 
 $(document).ready(function() {
     $('#javascript-area').text("Hello from javascript!")
