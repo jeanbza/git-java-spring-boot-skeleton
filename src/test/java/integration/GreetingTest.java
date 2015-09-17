fc3ec250f162d79947342cc8574cf0b0632851f0
Jean de Klerk <jadekler@gmail.com>
2015-09-16 21:33:17 -0600

    Switch to web integration test

--- src/test/java/integration/GreetingTest.java
+++ src/test/java/integration/GreetingTest.java
@@ -10,14 +10,12 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.*;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
-@IntegrationTest("server.port:0")
+@WebIntegrationTest("server.port:0")
 public class GreetingTest extends FluentTest {
     @Value("${local.server.port}")
     private int port;
